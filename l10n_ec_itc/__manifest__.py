2d9580ac07100e972413eaa03b36bbdc1734ce70
jamil jaramillo <innova_ec@hotmail.com>
2017-07-25 17:56:47 -0500

    Informacion de paquete

--- l10n_ec_itc/__manifest__.py
+++ l10n_ec_itc/__manifest__.py
@@ -1,11 +1,16 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-# Copyright (C) 2017 ITC Store
+# Copyright (C) 2017 ITC Tecnologia
 
 {
     'name': 'ITC - Accounting',
+	'summary': """NIIF SUPERCIAS Plan de Cuentas.""",
     'version': '1.2',
+	'author': "ITC Tecnologia",
+    'maintainer': 'Fabrica de Software Libre',
+    'website': 'http://www.itc.ec',
+    'license': 'AGPL-3',
     'category': 'Localization',
     'description': """
 Modulo Base Odoo que inicializa el Plan de Cuentas para Ecuador.
