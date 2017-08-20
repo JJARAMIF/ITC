5c8d7e1a6554938d7e5550bc95213d2de66ea9e1
jamil jaramillo <innova_ec@hotmail.com>
2017-08-20 17:29:05 -0500

    ok

--- l10n_ec_tax/__manifest__.py
+++ l10n_ec_tax/__manifest__.py
@@ -26,6 +26,7 @@ Plan de Impuestos Ecuador.
         'data/account.tax.group.csv',
         'data/account.tax.template.csv',
         'data/account.fiscal.position.csv',
+		'data/account_chart_template.yml',
         'security/ir.model.access.csv',
         'view/views.xml',
         'view/account_tax_report.xml'
